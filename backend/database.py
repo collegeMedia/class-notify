792d36118bc2c5d2a950a62d812a03454fb5c667
lobsam <sanduplobzang@gmail.com>
2026-02-20 22:59:21 +0530

    small change

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Replace with your actual database URL
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./university.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
